77a4dc35c230d40b4f29853788dd695b0860cc94
Otman Heddouch <85652350+otman-ai@users.noreply.github.com>
2023-05-12 13:33:41 +0200

    Update detect_app.py

--- detect_app.py
+++ detect_app.py
@@ -2,6 +2,7 @@ from ultralytics import YOLO
 import streamlit as st
 import cv2
 import numpy as np
+from skimage.io import imsave
 st.title("Hi,This is only test web application for extracting the Wall from image")
 st.write("Made by otman heddouch")
 st.write("Please upload image of house with Roof ,then the Wamm extracted from the image will be shown above after seconds.")
@@ -28,10 +29,12 @@ if image_streamlit != None:
     for result in img:
         mask = result.masks.cpu().numpy()
         masks = mask.masks.astype(bool)
+        dummy_image = np.zeros((masks.shape[0], masks.shape[1], 3), dtype=np.uint8)
+        dummy_image[masks] = [255, 255, 255]
+        imsave('mask.png', dummy_image)
         ori_img = result.orig_img
         new = np.ones_like(ori_img, dtype=np.uint8)
-        cv2.imwrite("b.png",masks)
-        siding_mask = cv2.imread('b.png', 0)  # Ensure the mask is grayscale (single channel)
+        siding_mask = cv2.imread('mask.png', 0)  # Ensure the mask is grayscale (single channel)
         siding_mask_3ch = cv2.cvtColor(siding_mask, cv2.COLOR_GRAY2BGR)
         new_color = np.array([0, 255, 0])  # Green color, you can adjust the values accordingly
         modified_image = cv2.addWeighted(original_img, 1.0, siding_mask_3ch, 0.5, 0)
