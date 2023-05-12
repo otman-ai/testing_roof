d49e5ca4f14b154bcf0e56eee1d63bcea85719ba
Otman Heddouch <85652350+otman-ai@users.noreply.github.com>
2023-05-12 13:43:18 +0200

    Update detect_app.py

--- detect_app.py
+++ detect_app.py
@@ -29,19 +29,22 @@ if image_streamlit != None:
     for result in img:
         mask = result.masks.cpu().numpy()
         masks = mask.masks.astype(bool)
-        dummy_image = np.zeros((masks.shape[0], masks.shape[1], 3), dtype=np.uint8)
-        dummy_image[masks] = [255, 255, 255]
-        imsave('mask.png', dummy_image)
+        #dummy_image = np.zeros((masks.shape[0], masks.shape[1], 3), dtype=np.uint8)
+        #dummy_image[masks] = [255, 255, 255]
+        #imsave('mask.png', masks)
         ori_img = result.orig_img
         new = np.ones_like(ori_img, dtype=np.uint8)
-        siding_mask = cv2.imread('mask.png', 0)  # Ensure the mask is grayscale (single channel)
-        siding_mask_3ch = cv2.cvtColor(siding_mask, cv2.COLOR_GRAY2BGR)
+        #siding_mask = cv2.imread('mask.png', 0)  # Ensure the mask is grayscale (single channel)
+        #siding_mask_3ch = cv2.cvtColor(siding_mask, cv2.COLOR_GRAY2BGR)
         new_color = np.array([0, 255, 0])  # Green color, you can adjust the values accordingly
         modified_image = cv2.addWeighted(original_img, 1.0, siding_mask_3ch, 0.5, 0)
         modified_image = cv2.addWeighted(modified_image, 1.0, new_color, 0.5, 0)
         for m in masks:
             new[m] = ori_img[m]
 
-        cv2.imwrite("h.jpg",new)
+        cv2.imwrite("h.png",new)
+        siding_mask_3ch = cv2.cvtColor(new, cv2.COLOR_GRAY2BGR)
+        modified_image = cv2.addWeighted(ori_img, 1.0, siding_mask_3ch, 0.5, 0)
+        modified_image = cv2.addWeighted(modified_image, 1.0, new_color, 0.5, 0)
         st.image(modified_image)
 
