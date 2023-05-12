304e16aab9fff28b752e1a914b34bd33037c757b
Otman Heddouch <85652350+otman-ai@users.noreply.github.com>
2023-05-12 13:26:31 +0200

    Update detect_app.py

--- detect_app.py
+++ detect_app.py
@@ -24,14 +24,21 @@ if image_streamlit != None:
     img = model.predict(source="image.jpg",
                         stream=True, retina_masks=True)
     new = None
+    original_img = cv2.imread("image.jpg")
     for result in img:
         mask = result.masks.cpu().numpy()
         masks = mask.masks.astype(bool)
         ori_img = result.orig_img
         new = np.ones_like(ori_img, dtype=np.uint8)
+        cv2.imwrite("b.png",masks)
+        siding_mask = cv2.imread('b.png', 0)  # Ensure the mask is grayscale (single channel)
+        siding_mask_3ch = cv2.cvtColor(siding_mask, cv2.COLOR_GRAY2BGR)
+        new_color = np.array([0, 255, 0])  # Green color, you can adjust the values accordingly
+        modified_image = cv2.addWeighted(original_img, 1.0, siding_mask_3ch, 0.5, 0)
+        modified_image = cv2.addWeighted(modified_image, 1.0, new_color, 0.5, 0)
         for m in masks:
             new[m] = ori_img[m]
 
         cv2.imwrite("h.jpg",new)
-        st.image("h.jpg")
+        st.image(modified_image)
 
